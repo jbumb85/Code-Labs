17da7e11d52d0308f1c4722a6d2c96164be97fc3
jbumb85 <111322252+jbumb85@users.noreply.github.com>
2022-09-11 21:08:44 -0500

    added ex01 and created ex02
    added exercise01 to exercise 01 folder

---
+++ class 07/exercise02/exercise02.js
@@ -0,0 +1,39 @@
+// # Exercise #2: Intermediate
+
+// Closures are functions that remember their lexical environments. Lexical environments mean the environment in which the function was declared.
+
+// ******************************************************************
+
+//     function parent(x) {
+//     return function closure() {
+//         // Closure is declared here.
+//         return x;
+//     };
+//     }
+
+//     const remember = parent("remembers me");
+//     // Seems like the variable x would be gone after
+//     // parent is executed, but it's not.
+
+//     closure();
+//     // Return "remembers me"
+
+
+// ******************************************************************
+    
+// **Aim:** Fix the code to correctly implement the callback.
+
+
+    function parent(x) {
+    return function closure() {
+        // Closure is declared here.
+       return x;
+    };
+    }
+
+    const remember = parent("remembers me");
+    // Seems like the variable x would be gone after
+    // parent is executed, but it's not.
+
+    remember();
+   
