b6ec7b772b85a58fa83144078d30b1f036a7f819
jbumb85 <111322252+jbumb85@users.noreply.github.com>
2022-09-12 11:33:09 -0500

    Create exercise09.js

---
+++ Class 07/exercise09/exercise09.js
@@ -0,0 +1,17 @@
+// How would you find the greatest common divisor of two numbers?
+
+function greatestCommonDivisor(a, b) {
+  if (typeof a !== "number" || typeof b !== "number") return false;
+  a = Math.abs(a);
+  b = Math.abs(b);
+  while (b) {
+    var t = b;
+    b = a % b;
+    a = t;
+  }
+  return a;
+}
+
+console.log(greatestCommonDivisor(14, 21)); // returns 7
+console.log(greatestCommonDivisor(69, 169)); // returns 1
+console.log(greatestCommonDivisor(63, 9)); // returns 9
