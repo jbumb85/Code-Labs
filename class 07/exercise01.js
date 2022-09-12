c01c116340ceb0028707c86aca00e9e515da6779
jbumb85 <111322252+jbumb85@users.noreply.github.com>
2022-09-11 20:33:31 -0500

    Create exercise01.js

---
+++ class 07/exercise01.js
@@ -0,0 +1,12 @@
+// Aim: Write a function redundantReturn that takes in a string a parameter named str. This function should return another function that returns a message using the str variable. Print that message to the developer console.
+
+function redundantReturn(str) {
+  return function(){
+    console.log(`${str}, my name is John`);
+  };
+}
+
+let newRedundantReturn = redundantReturn("Hello");
+
+newRedundantReturn();
+
