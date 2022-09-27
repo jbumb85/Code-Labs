dfbff9b4bdca88e4e79ed95e9e4081d945884d1f
jbumb85 <111322252+jbumb85@users.noreply.github.com>
2022-09-26 19:45:03 -0500

    created class12exercise02

---
+++ Class 12/Exercise02/basic-event-binding-exercise/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'basic-event-binding-exercise';
+
+  logOnClick() {
+    console.log("I've been clicked!!")
+  }
+}
